e5f40028c10fcce3738561e85f4a9b9965a7dcb2
Jensen <z5364270@unsw.edu.au>
2024-04-01 15:52:48 +1100

    refactored user profile

--- frontend/src/components/user/UserPage.tsx
+++ frontend/src/components/user/UserPage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
+import { Route, Routes, useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import GetStarted from "./GetStarted";
 import UploadPage from "./UploadPage";
@@ -13,7 +13,11 @@ import {
   getXmlData,
   sendInvoicesByNames,
 } from "../../service/service";
-import { Button, Form } from 'react-bootstrap';
+import {
+  Button, Checkbox, FormControlLabel, TextField,
+  Typography, Grid, AppBar, Toolbar, Paper, Link, Box
+} from '@mui/material';
+import { Container } from "react-bootstrap";
 
 function SendUI({ invoices, setShowSendUI }: { invoices: EInvoiceItem[], showSendUI: boolean, setShowSendUI: Function }) {
   const authContext = useContext(AuthContext);
@@ -25,16 +29,20 @@ function SendUI({ invoices, setShowSendUI }: { invoices: EInvoiceItem[], showSen
   return (
     <>
       <div>
-        <h2>Send Checked Invoices</h2>
-        <Form onSubmit={(e) => { e.preventDefault() }}>
-          <Form.Group>
-            <Form.Label>Recipient emails (comma separated):</Form.Label>
-            <Form.Control type='text' value={emailListStr} onChange={(e) => setEmailListStr(e.target.value)} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>From:</Form.Label>
-            <Form.Control type='text' value={from} onChange={(e) => setFrom(e.target.value)} />
-          </Form.Group>
+        <Typography variant="h5">Send Checked Invoices</Typography>
+        <form onSubmit={(e) => { e.preventDefault() }}>
+          <TextField
+            fullWidth
+            label="Recipient emails (comma separated)"
+            value={emailListStr}
+            onChange={(e) => setEmailListStr(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            label="From"
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+          />
           <Button onClick={async () => {
             const emails = emailListStr.split(',').filter(e => e !== '');
             if (emails.length == 0) {
@@ -65,7 +73,7 @@ function SendUI({ invoices, setShowSendUI }: { invoices: EInvoiceItem[], showSen
 
           }} disabled={buttonText !== 'SEND'}>{buttonText}</Button>
           <Button disabled={buttonText !== 'SEND'} onClick={() => setShowSendUI(false)}>Cancel</Button>
-        </Form>
+        </form>
 
       </div>
     </>
@@ -109,135 +117,190 @@ function Dashboard() {
 
   return (
     <>
-      <h1>Welcome, {authContext.currentUser?.username}</h1>
-      <div>
-        <h2>Profile</h2>
-        <div>Username: {authContext.currentUser?.username}</div>
-        <Button onClick={() => {
-          authContext.setCurrentUser(null);
-          navigate("/");
-        }}>Logout</Button>
-      </div>
-      <h2>Your Invoices</h2>
-      <Button onClick={() => {
-        navigate("/user/get-started");
-      }}>Get Started</Button>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
+          {authContext.currentUser == null ? (
+            <>
+              <Button variant="contained" color="primary" href="/login" role="button">
+                Sign In
+              </Button>
+              <Button variant="contained" color="primary" href="/register" role="button">
+                Sign Up
+              </Button>
+            </>
+          ) : (
+            // DOESNT ACTUALLY LOG A USER OUT!!! FIX LATER!
+            <Button color="inherit" href="/" role="button" onClick={() => {
+              authContext.setCurrentUser(null);
+              navigate("/");
+            }}>
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <div style={{ marginBottom: 25 }}></div>
+      <Grid container spacing={0} justifyContent={"space-between"} marginLeft={0} height={"auto"} marginRight={0}>
+        <Paper elevation={5} sx={{ paddingLeft: 3, paddingTop: 2, width: 400, paddingRight: 3, height: "auto", marginTop: -3, }}>
+          <Grid item xs={6}>
+            <Typography variant="h3">Welcome, {authContext.currentUser?.username}!</Typography>
+            {/* NO USER MANAGEMENT PAGE */}
+            <br />
+            <Link href="/user-profile" variant="body2">
+              Manage account
+            </Link>➡️
+          </Grid>
+          <br />
+          <Container>
+            <Typography variant="h6">
+              Current plan: Premium <br />
+            </Typography>
+            <Typography variant="h6">
+              Your team:
+            </Typography>
+            <Box bgcolor={"#cde6f7"} minHeight={250} display="flex" justifyContent="center" alignItems="center">
+              <Typography variant="body2">
+                You are currently not in a team
+              </Typography>
+            </Box>
+          </Container>
+        </Paper >
+        <Grid item xs={6}>
+          <Paper elevation={5} sx={{ paddingLeft: 3, paddingTop: 2, height: "auto", paddingRight: 7, marginTop: -3, width: "auto" }}>
+            <Typography variant="h4">Your Invoices</Typography>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => {
+                navigate("/user/get-started");
+              }}
+            >
+              Get Started
+            </Button>
+            {invoices.length == 0 ? (
+              <div>None</div>
+            ) : (
+              invoices.map((invoice, i) => (
+                <div key={invoice.id}>
+                  {invoice.name}
+                  <Button onClick={() => {
+                    window.open(`/user/view-invoice/${invoice.name}`);
+                  }}>View XML</Button>
 
-      {invoices.length == 0 ? (
-        <div>None</div>
-      ) : (
-        invoices.map((invoice, i) => (
-          <div key={invoice.id}>
-            {invoice.name}
+                  <Button onClick={async () => {
+                    changePdfButtonMsg("fetching xml...", i);
+                    const xmlData = await getXmlData(
+                      user!.username,
+                      user!.password,
+                      invoice.name
+                    );
+                    console.log(xmlData);
+
+                    changePdfButtonMsg("generating...", i);
+                    const link = await getPdfLink(
+                      user!.username,
+                      user!.password,
+                      xmlData
+                    );
+                    if (!link) {
+                      changePdfButtonMsg("an error occured :(", i);
+                      setTimeout(() => changePdfButtonMsg("generate pdf", i), 1000);
+                      return;
+                    }
+
+                    changePdfButtonMsg("generate pdf", i);
+                    setTimeout(() => window.open(link), 100);
+                  }}>{invoice.pdfGenMsg}</Button>
+
+                  <FormControlLabel
+                    control={<Checkbox
+                      checked={invoice.checked}
+                      onChange={(e) => {
+                        const invoices_ = [...invoices];
+                        invoices_[i].checked = e.target.checked;
+                        setInvoices(invoices_);
+                      }}
+                    />}
+                    label="Checked"
+                  />
+                </div>
+              ))
+            )}
+
+            <Button>Download</Button>
+
             <Button onClick={() => {
-              window.open(`/user/view-invoice/${invoice.name}`);
-            }}>View XML</Button>
-
-            <Button onClick={async () => {
-              changePdfButtonMsg("fetching xml...", i);
-              const xmlData = await getXmlData(
-                user!.username,
-                user!.password,
-                invoice.name
-              );
-              console.log(xmlData);
-
-              changePdfButtonMsg("generating...", i);
-              const link = await getPdfLink(
-                user!.username,
-                user!.password,
-                xmlData
-              );
-              if (!link) {
-                changePdfButtonMsg("an error occured :(", i);
-                setTimeout(() => changePdfButtonMsg("generate pdf", i), 1000);
+              if (!invoices.find(i => i.checked)) {
                 return;
               }
-
-              changePdfButtonMsg("generate pdf", i);
-              setTimeout(() => window.open(link), 100);
-            }}>{invoice.pdfGenMsg}</Button>
-
-            <Form.Check
-              type="checkbox"
-              checked={invoice.checked}
-              onChange={(e) => {
-                const invoices_ = [...invoices];
-                invoices_[i].checked = e.target.checked;
-                setInvoices(invoices_);
-              }}
-            />
-          </div>
-        ))
-      )}
-
-      <Button>Download</Button>
-
-      <Button onClick={() => {
-        if (!invoices.find(i => i.checked)) {
-          return;
-        }
-        setShowSendUI(true);
-      }}>Send</Button>
+              setShowSendUI(true);
+            }}>Send</Button>
 
 
-      <Button onClick={() => {
-        const numItems = invoices.filter((invoice) => invoice.checked).length;
-        if (numItems == 0) return;
-        setDeleteConfirmation({
-          state: "shown",
-          numItems: numItems,
-        });
-      }}>Delete</Button>
+            <Button onClick={() => {
+              const numItems = invoices.filter((invoice) => invoice.checked).length;
+              if (numItems == 0) return;
+              setDeleteConfirmation({
+                state: "shown",
+                numItems: numItems,
+              });
+            }}>Delete</Button>
 
-      {showSendUI && <SendUI invoices={invoices} showSendUI={showSendUI} setShowSendUI={setShowSendUI} />}
+            {showSendUI && <SendUI invoices={invoices} showSendUI={showSendUI} setShowSendUI={setShowSendUI} />}
 
-      {deletedConfirmation.state != "hidden" && (
-        <div>
-          {deletedConfirmation.state == "shown" ? (
-            <>
-              <h2>Delete Items</h2>
-              <div>Delete these {deletedConfirmation.numItems} items?'</div>
-            </>
+            {deletedConfirmation.state != "hidden" && (
+              <div>
+                {deletedConfirmation.state == "shown" ? (
+                  <>
+                    <Typography variant="h5">Delete Items</Typography>
+                    <div>Delete these {deletedConfirmation.numItems} items?'</div>
+                  </>
 
-          ) : (
-            <div>Loading</div>
-          )}
-          <Button onClick={async () => {
-            const names = invoices
-              .filter((invoice) => invoice.checked)
-              .map((invoice) => invoice.name);
-            setDeleteConfirmation({
-              ...deletedConfirmation,
-              state: "loading",
-            });
-            await deleteInvoicesFromUser(
-              user!.username,
-              user!.password,
-              names
-            );
+                ) : (
+                  <div>Loading</div>
+                )}
+                <Button onClick={async () => {
+                  const names = invoices
+                    .filter((invoice) => invoice.checked)
+                    .map((invoice) => invoice.name);
+                  setDeleteConfirmation({
+                    ...deletedConfirmation,
+                    state: "loading",
+                  });
+                  await deleteInvoicesFromUser(
+                    user!.username,
+                    user!.password,
+                    names
+                  );
 
-            setInvoices(invoices.filter((invoice) => !invoice.checked));
-            setDeleteConfirmation({
-              ...deletedConfirmation,
-              state: "hidden",
-            });
-          }}>Yes</Button>
-          <Button onClick={() =>
-            setDeleteConfirmation({ ...deletedConfirmation, state: "hidden" })
-          }>No</Button>
-        </div>
-      )}
-    </>
-  );
+                  setInvoices(invoices.filter((invoice) => !invoice.checked));
+                  setDeleteConfirmation({
+                    ...deletedConfirmation,
+                    state: "hidden",
+                  });
+                }}>Yes</Button>
+                <Button onClick={() =>
+                  setDeleteConfirmation({ ...deletedConfirmation, state: "hidden" })
+                }>No</Button>
+              </div>
+            )}
+          </Paper>
+        </Grid>
+      </Grid >
+    </>);
 }
 
 export function NotLoggedIn() {
   return (
     <>
-      <h1>You are not logged in</h1>
-      <p>You must log in to access features</p>
-      <Link to="/">Back</Link>
+      <Typography variant="h1">You are not logged in</Typography>
+      <Typography>You must log in to access features</Typography>
+      <Link href="/">Back</Link>
     </>
   );
 }
@@ -265,8 +328,8 @@ function InvoiceView() {
 
   return (
     <>
-      <h1>{invoiceName}</h1>
-      <pre>{xmlData}</pre>
+      <Typography variant="h1">{invoiceName}</Typography>
+      <Typography>{xmlData}</Typography>
     </>
   );
 }
