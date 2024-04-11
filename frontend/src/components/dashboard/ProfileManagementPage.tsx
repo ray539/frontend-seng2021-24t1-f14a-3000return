e035d61f2d470aedb8e16bbd4d6c951cc8caa5ef
Ray <ray.y.wang@student.unsw.edu.au>
2024-04-11 17:20:57 +1000

    complete frontend form profile management page

--- frontend/src/components/dashboard/ProfileManagementPage.tsx
+++ frontend/src/components/dashboard/ProfileManagementPage.tsx
@@ -4,17 +4,25 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContextProvider";
 import { DashBoardHeader } from "./DashboardPage";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 function BackButton() {
+  const navigate = useNavigate();
   return (
     <Box sx={{
         background: 'black',
         color: 'white',
         padding: '0.5em',
         borderRadius: '1em',
-        display: 'flex'
-      }}>
+        display: 'flex',
+        userSelect: 'none',
+        '&:hover': {
+          cursor: 'pointer'
+        }
+      }}
+      onClick={()=>navigate('/user')}  
+    >
       <img src="/images/back-arrow.png" style={{width: '30px', marginRight: '0.5em'}}></img>
       <Typography sx={{display: 'inline'}}>Back</Typography>
     </Box>
@@ -27,15 +35,19 @@ function DropDown({text, tc, bgc, element, showElement, setShowElement}: {text: 
   return (
     <>
         <Container sx={{
-          border: '1px solid black', 
           width: '50%', 
-          marginTop: '1em',
-          marginBottom: '1em',
+          marginTop: '2em',
+          marginBottom: '2em',
           display: 'flex', 
           justifyContent:'center', 
           userSelect: 'none',
           backgroundColor: bgc,
           color: tc,
+          padding: '0.5em',
+          borderRadius: '1em',
+          '&:hover': {
+            cursor: 'pointer'
+          }
         }}
           onClick={() => {
             setShowElement(!showElement)
@@ -59,7 +71,8 @@ function ChangeEmailForm({setShowElement}: {setShowElement: Function}) {
   return (
     <>
       <Container sx={{
-        border: '1px solid black', 
+        border: '1px solid #dddddd',
+        borderRadius: '1em',
         maxWidth: '75%',
         marginTop: '1em', 
         marginBottom: '1em', 
@@ -84,7 +97,10 @@ function ChangeEmailForm({setShowElement}: {setShowElement: Function}) {
           borderRadius: '0.5em',
           color: 'white',
           backgroundColor: 'black',
-          userSelect: 'none'
+          userSelect: 'none',
+          '&:hover': {
+            cursor: 'pointer'
+          }
         }}
           onClick={() => setShowElement(false)}
         >
@@ -143,7 +159,8 @@ function ChangePasswordForm({setShowElement}: {setShowElement: Function}) {
   return (
     <>
       <Container sx={{
-        border: '1px solid black', 
+        border: '1px solid #dddddd',
+        borderRadius: '1em',
         maxWidth: '75%',
         marginTop: '1em', 
         marginBottom: '1em', 
@@ -167,7 +184,10 @@ function ChangePasswordForm({setShowElement}: {setShowElement: Function}) {
           borderRadius: '0.5em',
           color: 'white',
           backgroundColor: 'black',
-          userSelect: 'none'
+          userSelect: 'none',
+          '&:hover': {
+            cursor: 'pointer'
+          }
         }}
           onClick={() => setShowElement(false)}
         >
@@ -218,12 +238,78 @@ function ChangePasswordForm({setShowElement}: {setShowElement: Function}) {
   )
 }
 
+
+function DeleteAccountForm({setShowElement}: {setShowElement: Function}) {
+  const [username, setUsername] = useState('')
+  const [pass, setPass] = useState('')
+
+  return (
+    <>
+      <Container sx={{
+        border: '1px solid #dddddd',
+        borderRadius: '1em',
+        maxWidth: '100%',
+        marginTop: '1em', 
+        marginBottom: '1em',
+        position: 'relative',
+        paddingTop: '2em'
+        }}>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          console.log('submited');
+          
+        }}>
+
+        
+        <Typography component={'h1'} sx={{width: 'fit-content', margin: 'auto', fontSize: '20px', fontWeight: '600'}}>
+          Are you sure you wish to delete your account?
+        </Typography>
+        <Typography component={'h1'} sx={{width: 'fit-content', margin: 'auto', fontSize: '15px', textAlign: 'center'}}>
+          This action is permanent and cannot be undone. To confirm, login once again.
+        </Typography>
+    
+        <TextField
+          fullWidth
+          label={'username'}
+          value={username}
+          onChange={(e) => {setUsername(e.target.value)}}
+          size='small'
+          required
+          sx={{ mt: 2, mb: 2 }}
+        >
+        </TextField>
+
+        <TextField
+          fullWidth
+          label={'password'}
+          type='password'
+          value={pass}
+          onChange={(e) => {setPass(e.target.value)}}
+          size='small'
+          required
+          sx={{ mt: 2, mb: 2 }}
+        >
+        </TextField>
+
+        <Container sx={{display: 'flex', justifyContent: 'center'}}>
+          <Button variant='contained' type='submit' color='error' sx={{margin: '1em'}}>Delete</Button>
+
+          <Button variant='contained' sx={{margin: '1em'}} onClick={() => setShowElement(false)}>Cancel</Button>
+        </Container>
+        </form>
+      </Container>
+
+    </>
+  )
+}
+
 export default function ProfileManagementPage() {
   const authContext = useContext(AuthContext);
   const user = authContext.currentUser
 
   const [showChangeEmail, setShowChangeEmail] = useState(false)
   const [showChangePass, setShowChangePass] = useState(false)
+  const [showDeleteAcc, setShowDeleteAcc] = useState(false)
 
   return (
     <>
@@ -236,8 +322,8 @@ export default function ProfileManagementPage() {
         <BackButton></BackButton>
       </Box>
 
-      <Container sx={{border:'1px solid black', marginTop: '1em'}} maxWidth='sm'>
-        <Box sx={{border: '1px solid black', width: 'fit-content',  margin: 'auto', display: 'flex', alignItems: 'center'}}>
+      <Container sx={{border:'1px solid #dddddd', backgroundColor: '#fcfcfc', marginTop: '1em', paddingTop: '1em'}} maxWidth='sm'>
+        <Box sx={{ width: 'fit-content',  margin: 'auto', display: 'flex', alignItems: 'center'}}>
           <img src="/images/empty-profile.png" style={{width: '50px', marginRight: '0.5em'}}></img>
         <Typography fontSize={20}>{user?.username}</Typography>
         </Box>
@@ -246,9 +332,10 @@ export default function ProfileManagementPage() {
 
         <DropDown setShowElement={setShowChangePass} showElement={showChangePass} text={"Change password"} tc={"white"} bgc={"black"} element={<ChangePasswordForm setShowElement={setShowChangePass}/>} ></DropDown>
 
-        <Box sx={{border: '1px solid black', width: '50%',  margin: 'auto', display: 'flex', justifyContent:'center'}}>
+        {/* <Box sx={{border: '1px solid black', width: '50%',  margin: 'auto', display: 'flex', justifyContent:'center'}}>
           <Typography>Delete account</Typography>
-        </Box>
+        </Box> */}
+        <DropDown setShowElement={setShowDeleteAcc} showElement={showDeleteAcc} text={"Delete account"} tc='white' bgc='red' element={<DeleteAccountForm setShowElement={setShowDeleteAcc}/>} />
 
       </Container>
 
