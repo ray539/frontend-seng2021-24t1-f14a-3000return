76e21a2856996df2bfb34a16e18b11580fae845c
Joshua Minh Pham <61824216+JPhamerino@users.noreply.github.com>
2024-04-17 11:52:45 +1000

    Complete validate page (?)

--- frontend/src/components/dashboard/InvoicesBox.tsx
+++ frontend/src/components/dashboard/InvoicesBox.tsx
@@ -27,162 +27,162 @@ import { evaluateString } from "./buttons/TagSelectionEvaluator";
 const buttonWidth = "65%";
 
 function Header({invoices}: {invoices: EInvoiceItem[]}) {
-	return (
-		<>
-			<Grid
-				container
-				justifyContent={"space-between"}
-				alignItems={"center"}
+  return (
+    <>
+      <Grid
+        container
+        justifyContent={"space-between"}
+        alignItems={"center"}
         height={"auto"}
-			>
-				<Grid item xs>
-					<Typography variant="h4" fontWeight={"bold"}>Invoices</Typography>
-				</Grid>
-				<Grid item width={buttonWidth}>
-					<GetStartedButton invoices={invoices} />
-				</Grid>
-			</Grid>
-		</>
-	);
+      >
+        <Grid item xs>
+          <Typography variant="h4" fontWeight={"bold"}>Invoices</Typography>
+        </Grid>
+        <Grid item width={buttonWidth}>
+          <GetStartedButton invoices={invoices} />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 function Buttons({
-	search, 
-	setSearch,
-	tagSelectionTxt,
-	setTagSelectionTxt,
-	invoices,
-	setInvoices
+  search, 
+  setSearch,
+  tagSelectionTxt,
+  setTagSelectionTxt,
+  invoices,
+  setInvoices
 }: {search: string, setSearch: Function, tagSelectionTxt: string, setTagSelectionTxt: Function, invoices: EInvoiceItem[], setInvoices: Function}) {
-	return (
-		<>
-			<Grid
-				container
-				justifyContent={"space-between"}
-				alignItems={"center"}
+  return (
+    <>
+      <Grid
+        container
+        justifyContent={"space-between"}
+        alignItems={"center"}
         height={"auto"}
-			>
-				<Grid
-					item
-					xs
-					paddingRight={"8px"}
-				>
-					<Box sx={{display:'flex', justifyContent: 'space-between'}}>
-						<TextField 
-							label="Search invoices" 
-							type="search"
-							variant="outlined" 
-							size="small"
-							fullWidth
-							sx={{marginRight: '1em'}}
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-						/>
-						<TextField 
-							fullWidth 
-							sx={{marginRight: '1em'}} 
-							label="Search tags" 
-							size='small'
-							value={tagSelectionTxt}
-							onChange={(e) => {
-								let newVal = e.target.value.toUpperCase()
-								if (!/^[A-Z0-9_\-,|\(\)]*$/.test(newVal)) {
-									console.log('here');
-									return;
-								}
-								setTagSelectionTxt(newVal)
-							}}
-						/>
-					</Box>
-				</Grid>
-				<Grid 
-					container 
-					width={buttonWidth}
-					gap={"8px"}
-				>
-					<Grid item xs>
-						<DownloadButton invoices={invoices} />
-					</Grid>
-					<Grid item xs>
-						<SendButton invoices={invoices} />
-					</Grid>
-					<Grid item xs>
-						<DeleteButton invoices={invoices} setInvoices={setInvoices}/>
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+      >
+        <Grid
+          item
+          xs
+          paddingRight={"8px"}
+        >
+          <Box sx={{display:'flex', justifyContent: 'space-between'}}>
+            <TextField 
+              label="Search invoices" 
+              type="search"
+              variant="outlined" 
+              size="small"
+              fullWidth
+              sx={{marginRight: '1em'}}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <TextField 
+              fullWidth 
+              sx={{marginRight: '1em'}} 
+              label="Search tags" 
+              size='small'
+              value={tagSelectionTxt}
+              onChange={(e) => {
+                let newVal = e.target.value.toUpperCase()
+                if (!/^[A-Z0-9_\-,|\(\)]*$/.test(newVal)) {
+                  console.log('here');
+                  return;
+                }
+                setTagSelectionTxt(newVal)
+              }}
+            />
+          </Box>
+        </Grid>
+        <Grid 
+          container 
+          width={buttonWidth}
+          gap={"8px"}
+        >
+          <Grid item xs>
+            <DownloadButton invoices={invoices} />
+          </Grid>
+          <Grid item xs>
+            <SendButton invoices={invoices} />
+          </Grid>
+          <Grid item xs>
+            <DeleteButton invoices={invoices} setInvoices={setInvoices}/>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 function Invoice({
-		invoice,
-		i,
-		invoices,
-		setInvoices
-	} : {
-		invoice: EInvoiceItem, 
-		i: number, 
-		invoices: EInvoiceItem[], 
-		setInvoices: Function
-	}) {
+    invoice,
+    i,
+    invoices,
+    setInvoices
+  } : {
+    invoice: EInvoiceItem, 
+    i: number, 
+    invoices: EInvoiceItem[], 
+    setInvoices: Function
+  }) {
   const authContext = useContext(AuthContext);
   const user = authContext.currentUser;
 
-	async function getPDF() {
-		const xmlData = await getXmlData(
-			user!.username,
-			user!.password,
-			invoice.name
-		);
+  async function getPDF() {
+    const xmlData = await getXmlData(
+      user!.username,
+      user!.password,
+      invoice.name
+    );
 
-		// console.log(xmlData);
-		const link = await getPdfLink(
-			user!.username,
-			user!.password,
-			xmlData
-		);
+    // console.log(xmlData);
+    const link = await getPdfLink(
+      user!.username,
+      user!.password,
+      xmlData
+    );
 
-		setTimeout(() => window.open(link), 100);
-	}
+    setTimeout(() => window.open(link), 100);
+  }
 
-	return (
-		<>
-			<Grid 
-				container
-				wrap="nowrap"
+  return (
+    <>
+      <Grid 
+        container
+        wrap="nowrap"
         width={"100%"}
         paddingLeft={2}
         paddingRight={2}
         borderBottom={"1px solid grey"}
-			>
-				<Box width={"50%"} overflow={"hidden"}>
-					<FormControlLabel
-						control={
-							<Checkbox
-								checked={invoice.checked}
-								onChange={(e) => {
-									const invoices_ = [...invoices];
-									// get the invoice which has that name
-									invoices_[i].checked = e.target.checked;
-									setInvoices(invoices_);
-								}}
-							/>
-						}
-						label={invoice.name} // Set the label of the checkbox to be the name of the invoice
-						labelPlacement="end" // Align the label to the start of the checkbox
-					/>
-				</Box>
+      >
+        <Box width={"50%"} overflow={"hidden"}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={invoice.checked}
+                onChange={(e) => {
+                  const invoices_ = [...invoices];
+                  // get the invoice which has that name
+                  invoices_[i].checked = e.target.checked;
+                  setInvoices(invoices_);
+                }}
+              />
+            }
+            label={invoice.name} // Set the label of the checkbox to be the name of the invoice
+            labelPlacement="end" // Align the label to the start of the checkbox
+          />
+        </Box>
 
-				<Grid 
-					container
-					justifyContent={"flex-end"}
+        <Grid 
+          container
+          justifyContent={"flex-end"}
           width={"100%"}
           alignItems={"center"}
           wrap="nowrap"
-					gap={1}
-				>
-					<Box 
+          gap={1}
+        >
+          <Box 
             sx={{
               width: '100%',
               display: 'flex', 
@@ -203,19 +203,19 @@ function Invoice({
                 />
               )
             }
-					</Box>
+          </Box>
           <ManageTagButton invoices={invoices} setInvoices={setInvoices} index={i}/>
-					<Tooltip title="View eInvoice">
-						<IconButton 
-							aria-label="View"
-							onClick={() => {
-								window.open(`/user/view-invoice/${invoice.name}`);
-							}}
-						>
-							<VisibilityIcon />
-						</IconButton>
-					</Tooltip>
-					{user!.accountType !== 'Free' ? (
+          <Tooltip title="View eInvoice">
+            <IconButton 
+              aria-label="View"
+              onClick={() => {
+                window.open(`/user/view-invoice/${invoice.name}`);
+              }}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Tooltip>
+          {user!.accountType !== 'Free' ? (
               <Tooltip title="Generate PDF">
                 <IconButton
                   aria-label="PDF"
@@ -236,71 +236,71 @@ function Invoice({
                 </span>
               </Tooltip>
             )
-					}
-				</Grid>
-			</Grid>
-		</>
-	)
+          }
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 function Invoices({invoices, setInvoices}: {invoices: EInvoiceItem[], setInvoices: Function}) {
-	const [selectAll, setSelectAll] = useState(false);
-	const shownInvoices = invoices.filter(invoice => invoice.shown)
-	return (
-		<>
-			<Grid
-				item
-				display={"flex"}
-				flexDirection={"column"}
-				padding={"20px"}
+  const [selectAll, setSelectAll] = useState(false);
+  const shownInvoices = invoices.filter(invoice => invoice.shown)
+  return (
+    <>
+      <Grid
+        item
+        display={"flex"}
+        flexDirection={"column"}
+        padding={"20px"}
         paddingTop={1}
-				width={"100%"}
+        width={"100%"}
         height={"100%"}
         overflow={"auto"}
-				sx={{
-					bgcolor: "#F1E8FF",
-				}}
-			>
-				<Grid 
-					container 
-					wrap="nowrap"
-				>
-					<Box 
-						width={"100%"}
+        sx={{
+          bgcolor: "#F1E8FF",
+        }}
+      >
+        <Grid 
+          container 
+          wrap="nowrap"
+        >
+          <Box 
+            width={"100%"}
             paddingLeft={2}
             paddingRight={2}
-						borderBottom={"1px solid black"}
-					>
-						<FormControlLabel
-							control={
-								<Checkbox
-									checked={selectAll}
-									onChange={(e) => {
-										setSelectAll(e.target.checked);
-										shownInvoices.map((invoice) => invoice.checked = !selectAll)
-									}}
-								/>
-							}
-							label={"Name"} // Set the label of the checkbox to be the name of the invoice
-						/>
-					</Box>
-				</Grid>
+            borderBottom={"1px solid black"}
+          >
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={selectAll}
+                  onChange={(e) => {
+                    setSelectAll(e.target.checked);
+                    shownInvoices.map((invoice) => invoice.checked = !selectAll)
+                  }}
+                />
+              }
+              label={"Name"} // Set the label of the checkbox to be the name of the invoice
+            />
+          </Box>
+        </Grid>
 
 
-				{
+        {
           shownInvoices.map((invoice) => (
             <Invoice key={invoice.id} invoice={invoice} i={invoice.index} invoices={invoices} setInvoices={setInvoices}/>
           ))
-				}
-			</Grid>
-		</>
-	);
+        }
+      </Grid>
+    </>
+  );
 }
 
 function shouldShowInvoice(invoice: EInvoiceItem, tagSelectionTxt: string, search: string) {
-	const res1 = evaluateString(invoice, tagSelectionTxt) == 'true'
-	const res2 = RegExp(new RegExp(search, 'i')).test(invoice.name)
-	return res1 && res2
+  const res1 = evaluateString(invoice, tagSelectionTxt) == 'true'
+  const res2 = RegExp(new RegExp(search, 'i')).test(invoice.name)
+  return res1 && res2
 }
 
 export default function InvoicesBox() {
@@ -308,29 +308,29 @@ export default function InvoicesBox() {
   const user = authContext.currentUser;
   const [invoices, setInvoices] = useState<EInvoiceItem[]>([]);
   const [search, setSearch_] = useState("");
-	const [tagSelectionTxt, setTagSelectionTxt_] = useState('')
+  const [tagSelectionTxt, setTagSelectionTxt_] = useState('')
 
-	// set searchTxt and unchecks invoices which become invisible
-	function setSearch(value: string) {
-		const invoicesNew = [...invoices]
-		for (let invoice of invoicesNew) {
-			invoice.shown = shouldShowInvoice(invoice, tagSelectionTxt, value)
-			invoice.checked = false
-		}
-		setInvoices(invoicesNew)
-		setSearch_(value)
-	}
+  // set searchTxt and unchecks invoices which become invisible
+  function setSearch(value: string) {
+    const invoicesNew = [...invoices]
+    for (let invoice of invoicesNew) {
+      invoice.shown = shouldShowInvoice(invoice, tagSelectionTxt, value)
+      invoice.checked = false
+    }
+    setInvoices(invoicesNew)
+    setSearch_(value)
+  }
 
-	// set tagSelectionTxt and check
-	function setTagSelectionTxt(value: string) {
-		const invoicesNew = [...invoices]
-		for (let invoice of invoicesNew) {
-			invoice.shown = shouldShowInvoice(invoice, value, search)
-			invoice.checked = false
-		}
-		setInvoices(invoicesNew)
-		setTagSelectionTxt_(value)
-	}
+  // set tagSelectionTxt and check
+  function setTagSelectionTxt(value: string) {
+    const invoicesNew = [...invoices]
+    for (let invoice of invoicesNew) {
+      invoice.shown = shouldShowInvoice(invoice, value, search)
+      invoice.checked = false
+    }
+    setInvoices(invoicesNew)
+    setTagSelectionTxt_(value)
+  }
 
   useEffect(() => {
     console.log(user?.username, user?.password);
@@ -351,17 +351,17 @@ export default function InvoicesBox() {
       >
         <Header invoices={invoices}/>
         <Buttons 
-					search={search} 
-					setSearch={setSearch} 
-					invoices={invoices} 
-					setInvoices={setInvoices}
-					tagSelectionTxt={tagSelectionTxt}
-					setTagSelectionTxt={setTagSelectionTxt}
-				/>
+          search={search} 
+          setSearch={setSearch} 
+          invoices={invoices} 
+          setInvoices={setInvoices}
+          tagSelectionTxt={tagSelectionTxt}
+          setTagSelectionTxt={setTagSelectionTxt}
+        />
         <Invoices
-					invoices={invoices}
-					setInvoices={setInvoices}
-					/>
+          invoices={invoices}
+          setInvoices={setInvoices}
+          />
       </Grid>
     </>
   );
